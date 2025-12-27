5b0bdb75a4a3c06000ddfa0b5233b056db35b0fc
JuanVimon3 <jvillamonsalve@gmail.com>
2025-12-27 16:09:18 -0500

    add responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Menu, X } from "lucide-react"
 
 export default function Header() {
   const pathName = usePathname()
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const links = [
     { href: "/", label: "home" },
     { href: "/createAccount", label: "Crear cuenta" },
@@ -12,21 +16,45 @@ export default function Header() {
   ]
 
   return (
-    <header className="flex items-center justify-between bg-[#840705] text-[#F7F6F6] py-4 px-6">
-      <nav className=" lg:flex ml-0 mr-0 space-x-16 mx-auto font-source-sans">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={`${pathName === link.href ? "font-semibold underline" : ""}`}
+    <header className="bg-[#840705] text-[#F7F6F6] py-4 px-6 relative shadow-md">
 
-          >
-            {link.label}
-          </Link>
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
 
-        ))}
-      </nav>
-      <div className="absolute left-1/2 transform -translate-x-1/2 font-bold text-lg text-[#F7F6F6] "> Compra y venta de propiedades</div>
+        <h1 className="hidden md:block absolute left-1/2 transform -translate-x-1/2 text-lg lg:text-xl text-[#F7F6F6] "> Compra y venta de propiedades</h1>
+
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden focus:outline-none"
+        >
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+
+        <nav className={`
+        ${menuOpen ? "flex" : "hidden"}
+        flex-col md:flex md:flex-row md:space-x-16 absolute md:static top-16 letf-0 w-full md:w-auto bg-[#840705] md:bg-transparent text-center md:text-left transition-all duration-300 z-20
+        `}>
+
+          {links.map((link) =>
+            <Link
+              key={link.href}
+              href={link.href}
+              onClick={() => setMenuOpen(false)}
+              className={`
+              block py-2 md:py-0
+              ${pathName === link.href ? "font-semibold underline" : ""}
+              `}
+            >
+              {link.label}
+            </Link>
+          )}
+        </nav>
+
+      </div>
+
+      <h1 className="md:hidden text-center font-bold mt-2 text-base">
+        Compra y venta de propiedades
+      </h1>
+
     </header>
   )
 }
