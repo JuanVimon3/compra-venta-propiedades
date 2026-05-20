66cbf0bc28608202331a12354ada3516d8f82a2a
JuanVimon3 <jvillamonsalve@gmail.com>
2026-05-20 14:37:13 -0500

    WIP

--- src/actions/uploadAction.ts
+++ src/actions/uploadAction.ts
@@ -9,7 +9,9 @@ const storage = new Storage({
   projectId: process.env.GCP_PROJECT_ID,
   credentials: {
     client_email: process.env.GCP_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    private_key: process.env.GCP_PRIVATE_KEY
+      ? process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '').trim()
+      : undefined,
   },
 });
 
