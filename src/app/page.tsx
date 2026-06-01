c903ff25d66c42b232259fa9d1a9d8efd522e3f4
JuanVimon3 <jvillamonsalve@gmail.com>
2026-06-01 12:12:30 -0500

    Add filter functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // 1. ELIMINAMOS 'use client' para poder hacer el fetch directamente en el servidor
-import PropertyCard from "@/components/PropertyCard";
+import ListenerSelector from "@/components/ListenerSelector";
 import { Property } from "@/types";
 
 export default async function Home() {
@@ -13,11 +13,9 @@ export default async function Home() {
   console.log("Datos recibidos de la API:", data);
 
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+    <main>
       {/* 3. Mapeamos 'data' (lo que viene de la DB) en lugar del archivo estático */}
-      {data.map((property) => (
-        <PropertyCard key={property.idPropiedad} property={property}/>
-      ))}
+      <ListenerSelector properties={data} />
     </main>
   );
 }
